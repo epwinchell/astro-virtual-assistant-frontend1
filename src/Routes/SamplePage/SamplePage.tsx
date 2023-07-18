04e991084d096ed11b73cc7e9809781604cdfde8
Josejulio Martínez <josejulio@users.noreply.github.com>
2023-07-18 12:12:04 -0600

    Initial commit create-crc-app

---
+++ src/Routes/SamplePage/SamplePage.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react';
+
+import { Stack, StackItem, Title } from '@patternfly/react-core';
+import { Main } from '@redhat-cloud-services/frontend-components/Main';
+import { PageHeader, PageHeaderTitle } from '@redhat-cloud-services/frontend-components/PageHeader';
+
+
+/**
+ * A smart component that handles all the api calls and data needed by the dumb components.
+ * Smart components are usually classes.
+ *
+ * https://reactjs.org/docs/components-and-props.html
+ * https://medium.com/@thejasonfile/dumb-components-and-smart-components-e7b33a698d43
+ */
+const SamplePage = () => {
+  useEffect(() => {
+    insights?.chrome?.appAction?.('sample-page');
+  }, []);
+
+  return (
+    <React.Fragment>
+      <PageHeader>
+        <PageHeaderTitle title="Virtual assistant" />
+      </PageHeader>
+      <Main>
+        <Stack hasGutter>
+          <StackItem>
+            <Title headingLevel="h2" size="3xl">
+              {' '}
+              Virtual assistant UI goes here{' '}
+            </Title>
+          </StackItem>
+        </Stack>
+      </Main>
+    </React.Fragment>
+  );
+};
+
+export default SamplePage;
