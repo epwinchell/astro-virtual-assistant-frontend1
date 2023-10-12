f8dd5c39f48d8f19f8c3673e4470e46796a30ba2
Josejulio Martínez <josejulio@users.noreply.github.com>
2023-10-12 06:58:25 -0600

    Lint fix (#20)

--- src/Components/Message/UserMessageEntry.tsx
+++ src/Components/Message/UserMessageEntry.tsx
@@ -10,7 +10,7 @@ export const UserMessageEntry: FunctionComponent<MessageProps<UserMessage>> = ({
       <Split className="astro-user pf-u-align-items-flex-end pf-u-justify-content-flex-end">
         <SplitItem className="bubble pf-u-background-color-active-color-100">
           <TextContent className="pf-u-color-light-100 pf-u-font-size-sm">{message.content}</TextContent>
-      </SplitItem>
+        </SplitItem>
         <SplitItem>
           <Icon size="lg" className="pf-u-ml-sm">
             <OutlinedUserCircleIcon />
