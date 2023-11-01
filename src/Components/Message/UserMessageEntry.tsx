8d67534b82f20b37db31bcd41d27957f5c0f10f5
epwinchell <1287144+epwinchell@users.noreply.github.com>
2023-11-01 12:34:03 -0400

    Add header background / move user icon right (#48)

--- src/Components/Message/UserMessageEntry.tsx
+++ src/Components/Message/UserMessageEntry.tsx
@@ -8,13 +8,13 @@ export const UserMessageEntry: FunctionComponent<MessageProps<UserMessage>> = ({
   return (
     <>
       <Split className="astro-user pf-v5-u-mb-lg pf-v5-u-align-items-flex-start pf-v5-u-justify-content-flex-end">
-        <SplitItem>
-          <Icon size="lg" className="pf-v5-u-mr-md">
-            <OutlinedUserCircleIcon />
-          </Icon>
-        </SplitItem>
         <SplitItem className="bubble bubble-user">
           <TextContent className="pf-v5-u-color-300 pf-v5-u-font-size-sm">{message.content}</TextContent>
+        </SplitItem>
+        <SplitItem>
+          <Icon size="lg" className="pf-v5-u-ml-sm pf-v5-u-pt-xs">
+            <OutlinedUserCircleIcon />
+          </Icon>
         </SplitItem>
       </Split>
     </>
