7f94d465f668b3b600b2e4c96f8ac6e7353e41dd
Martin Maroši <marvusm.mmi@gmail.com>
2023-12-08 14:52:55 +0100

    RHCLOUD-29659 add badge tooltip (#85)

--- src/Components/AstroAvatar/AstroBadge.tsx
+++ src/Components/AstroAvatar/AstroBadge.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, ButtonVariant } from '@patternfly/react-core';
+import { Button, ButtonVariant, Tooltip } from '@patternfly/react-core';
 import BubbleIcon from '../../assets/badge-icon.svg';
 
 interface AstroAvatarProps {
@@ -8,8 +8,10 @@ interface AstroAvatarProps {
 
 export const AstroBadge: React.FunctionComponent<AstroAvatarProps> = ({ onClick }) => {
   return (
-    <Button className="astro-c-button-badge pf-v5-u-box-shadow-lg" onClick={onClick}>
-      <img className="astro__badge" src={BubbleIcon} alt="Launch virtual assistant" />
-    </Button>
+    <Tooltip position="left" content={<div>Virtual assistant</div>}>
+      <Button className="astro-c-button-badge pf-v5-u-box-shadow-lg" onClick={onClick}>
+        <img className="astro__badge" src={BubbleIcon} alt="Launch virtual assistant" />
+      </Button>
+    </Tooltip>
   );
 };
