614e7e96e699b856268394365209883acaef4634
Josejulio Martínez <josejulio@users.noreply.github.com>
2024-03-14 12:13:13 -0600

    Integration flow feedback (#118)
    * Integration flow feedback
     - anytime => any time
     - Wait 3 secs before opening a new tab
    
    * lint fix

--- src/Components/Banners/ConversationEndBanner.tsx
+++ src/Components/Banners/ConversationEndBanner.tsx
@@ -7,7 +7,13 @@ export const ConversationEndBanner: FunctionComponent<MessageProps<Banner>> = ()
   return (
     <>
       <TextContent className="banner__chat-end">
-        <Alert variant="info" isInline title="You can start a new conversation at anytime by typing below." component="h6" className="banner-alert" />
+        <Alert
+          variant="info"
+          isInline
+          title="You can start a new conversation at any time by typing below."
+          component="h6"
+          className="banner-alert"
+        />
       </TextContent>
     </>
   );
--- src/SharedComponents/AstroVirtualAssistant/CommandMessageProcessor.tsx
+++ src/SharedComponents/AstroVirtualAssistant/CommandMessageProcessor.tsx
@@ -5,8 +5,10 @@ import { feedbackCommandProcessor } from './CommandProcessor/FeedbackCommandProc
 import { thumbsCommandProcessor } from './CommandProcessor/ThumbsCommandProcessor';
 
 const openInNewTab = (url: string) => {
-  const newWindow = window.open(url, '_blank', 'noopener,noreferrer');
-  if (newWindow) newWindow.opener = null;
+  setTimeout(() => {
+    const newWindow = window.open(url, '_blank', 'noopener,noreferrer');
+    if (newWindow) newWindow.opener = null;
+  }, 3000);
 };
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
